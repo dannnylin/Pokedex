d48a8d17c28d19cb11da992ea53f30f2ae17594d
Danny Lin <danny.lin@nyu.edu>
2016-07-09 16:23:01 -0400

    Implement search filtering

--- Pokedex/Controller/HomeViewController.swift
+++ Pokedex/Controller/HomeViewController.swift
@@ -7,17 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class HomeViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var dataSource = [Pokemon]()
+    var filteredSource = [Pokemon]()
+    var searching = false
+    
+    var musicPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        searchBar.returnKeyType = .Done
+        initAudio()
         parsePokemonCSV()
+    }
+    
+    func initAudio() {
+        let path = NSBundle.mainBundle().pathForResource("music", ofType: ".mp3")!
+        do {
+            musicPlayer = try AVAudioPlayer(contentsOfURL: NSURL(string: path)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.volume = 0.5
+            musicPlayer.play()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
     }
     
     func parsePokemonCSV() {
@@ -36,6 +57,16 @@ class HomeViewController: UIViewController {
             print(err.debugDescription)
         }
     }
+    
+    @IBAction func musicButtonPressed(sender: UIButton) {
+        if musicPlayer.playing {
+            musicPlayer.stop()
+            sender.alpha = 0.25
+        } else {
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
+    }
 }
 
 extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
@@ -44,7 +75,11 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 718
+        if searching {
+            return filteredSource.count
+        } else {
+            return dataSource.count
+        }
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
@@ -52,7 +87,14 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
             fatalError()
         }
         
-        let pokemon = dataSource[indexPath.row]
+        let pokemon: Pokemon!
+        
+        if searching {
+            pokemon = filteredSource[indexPath.row]
+        } else {
+            pokemon = dataSource [indexPath.row]
+        }
+        
         cell.setupCell(pokemon)
         
         return cell
@@ -65,4 +107,24 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         return CGSize(width: 110, height: 110)
     }
+}
+
+extension HomeViewController: UISearchBarDelegate {
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == "" || searchBar.text == nil {
+            searching = false
+            view.endEditing(true)
+            collectionView.reloadData()
+        } else {
+            searching = true
+            let lower = searchBar.text!.lowercaseString
+            filteredSource = dataSource.filter({$0.name.rangeOfString(lower) != nil})
+            collectionView.reloadData()
+        }
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    
 }
